937787d344e06e64ec992acdb9fe0cb6834b7524
John Thompson <john@springframework.guru>
2021-05-09 13:56:03 -0400

    refactoring

--- src/test/java/guru/springframework/sfgrestbrewery/KbeRestBreweryApplicationTests.java
+++ src/test/java/guru/springframework/sfgrestbrewery/KbeRestBreweryApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SfgReactiveBreweryApplicationTests {
+public class KbeRestBreweryApplicationTests {
 
 	@Test
 	public void contextLoads() {
